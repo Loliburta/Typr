ec69102a5dc316f2f1b65e01ab466cc3e0dc6f80
Loliburta <mateusz.maranowski@protonmail.com>
2021-06-12 18:53:04 +0200

    added reset button and quick reset by tabbing + enter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { words } from "../utils/words";
 import useKeyPress from "../hooks/useKeyPress";
 import { Summary } from "./summary/Summary";
+import { Icon } from "@iconify/react";
+import resetIcon from "@iconify-icons/codicon/debug-restart";
 export const App = () => {
   const [typingTest, setTypingTest] = useState("");
   const [outgoingChars, setOutgoingChars] = useState("");
@@ -12,7 +14,11 @@ export const App = () => {
   const [mistakes, setMistakes] = useState(0);
   const [showSummary, setShowSummary] = useState(false);
   const [testLength, setTestLength] = useState(25);
+  const [accuracy, setAccuracy] = useState(0);
+
+  const outFocusRef = useRef(null);
   const reset = () => {
+    console.log("abc");
     const test = words
       .sort(() => 0.5 - Math.random())
       .slice(0, testLength)
@@ -26,7 +32,12 @@ export const App = () => {
   };
   useEffect(() => {
     reset();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [testLength]);
+  useEffect(() => {
+    if (testLength === 10) {
+    }
   }, [testLength]);
   useKeyPress((key: any) => {
     if (key === currentChar) {
@@ -41,12 +52,22 @@ export const App = () => {
         setTimeToWrite(
           Math.round(((Date.now() - timeToWrite) / 1000) * 100) / 100
         );
+        setAccuracy(
+          Math.floor((typingTest.length / (typingTest.length + mistakes)) * 100)
+        );
         setShowSummary(true);
       }
     } else {
       setMistakes(mistakes + 1);
     }
   });
+  const tabHandle = (key: any, outFocusRef: any) => {
+    if (key === "Enter") {
+      reset();
+    }
+    outFocusRef.current.focus();
+    outFocusRef.current.blur();
+  };
 
   return (
     <div className="app">
@@ -55,7 +76,9 @@ export const App = () => {
           Typr
         </p>
         <div className="app__options">
-          Words{" "}
+          <span tabIndex={-1} ref={outFocusRef}>
+            Words
+          </span>{" "}
           <span
             className={
               testLength === 10
@@ -92,9 +115,7 @@ export const App = () => {
         {showSummary ? (
           <Summary
             wpm={typingTest.length / 5 / (timeToWrite / 60)}
-            acc={Math.floor(
-              (typingTest.length / (typingTest.length + mistakes)) * 100
-            )}
+            acc={accuracy}
             time={timeToWrite}
           />
         ) : (
@@ -110,6 +131,16 @@ export const App = () => {
             </div>
           </>
         )}
+      </div>
+      <div className="app__bot">
+        <div
+          className="app__bot__icon"
+          tabIndex={0}
+          onKeyDown={({ key }) => tabHandle(key, outFocusRef)}
+          onClick={reset}
+        >
+          <Icon icon={resetIcon} flip="horizontal" />
+        </div>
       </div>
     </div>
   );
